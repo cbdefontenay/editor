0a8637c01fb2a6a7ef8bb84d45ae9d68ce4fde7c
cbdefontenay <cbdefontenay@gmail.com>
2025-01-20 22:59:00 +0100

    Premier Commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::{container, text, text_editor};
+use iced::{application, run, Element, Theme};
+
+fn main() -> iced::Result {
+    application("Editor", Editor::update, Editor::view).run()
+}
+
+#[derive(Default)]
+struct Editor {
+    content: text_editor::Content,
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Edit(text_editor::Action),
+}
+
+impl Editor {
+    fn new() -> Self {
+        Self {
+            content: text_editor::Content::with_text(include_str!("main.rs")),
+        }
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Edit(action) => {
+                self.content.perform(action);
+            }
+        }
+    }
+
+    fn view(&self) -> Element<'_, Message> {
+        let input: Element<'_, Message> = text_editor::<Message, Theme, iced::Renderer>(&self.content) // Explicit generic arguments
+            .on_action(Message::Edit)
+            .into();
+
+        container(input).padding(10).into()
+    }
+
+    fn theme(&self) -> Theme {
+        Theme::Dark
+    }
 }
